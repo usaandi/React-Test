ab64f9b9edee5c112f8852b9159439d6be4188e9
Kaur <kaurjoonas@hot.ee>
2020-05-21 12:26:03 +0300

    UseState argument

--- src/components/BodyComponent/Body.js
+++ src/components/BodyComponent/Body.js
@@ -1,15 +1,24 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
 
-class Body extends Component {
-    render() {
-        return (
-            <div>
-                <p>Hello world</p>
-                {this.props.userData.map(user => <div>{user.id} </div>)}
-            </div>
-        )
+const Body = props => {
+
+    let [nameValue, setName] = useState('');
+
+    const onChangeEvent = e =>{
+        setName(e.target.value)
     }
+
+    return (
+        <div>
+            <input onChange={onChangeEvent} type='text' value={nameValue}/>
+
+            <p>Hello world! your Name is: {nameValue}</p>
+            {props.userData.map((user,index) => <div key={index}>{user.id} </div>)}
+        </div>
+    )
+
+
 }
 
 
